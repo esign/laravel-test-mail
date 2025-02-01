74062e9f946d6e387ab90e90804c651db043112f
jordyvanderhaegen <jordy@esign.eu>
2025-02-01 23:11:15 +0100

    chore: remove unused package scaffolding

--- src/TestMail.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Esign\TestMail;
-
-class TestMail
-{
-}
--- src/TestMailServiceProvider.php
+++ src/TestMailServiceProvider.php
@@ -8,22 +8,5 @@ class TestMailServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([$this->configPath() => config_path('test-mail.php')], 'config');
-        }
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom($this->configPath(), 'test-mail');
-
-        $this->app->singleton('test-mail', function () {
-            return new TestMail;
-        });
-    }
-
-    protected function configPath(): string
-    {
-        return __DIR__ . '/../config/test-mail.php';
     }
 }
